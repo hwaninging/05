8be84ca7edff83f501e94de8d70f859a510ea993
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-02 15:34:28 +0900

    4. 덧셈 프로그램
    이해 잘 안댐.

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-// 2. 절대값 구하기 
+// 4. 덧셈 프로그램 
 int main(int argc, char *argv[])
 {
-  int x;
+  int x; 
+  int i; //for문 받아주기 위한 것 
+  int sum=0; //누적 더하기를 저장하는 저장소 
    
-  printf("정수 하나를 입력하시오 : ");
+  printf("input a number: ");
   scanf("%d", &x);
   
-   if (x>0) //x가 양수라는 조건식 
-      printf("절대값은 %i 입니다.\n", x); //숫자 넣는 %i 
-   else
-      printf("절대값은 %i 입니다.\n", -x); 
+  //3가지 넣어야 하니까 뭘 반복할지부터 하기
+  for(i=0;i<=x;i++) //i 더할때마다 i 하나씩 증가 
+  sum = sum + i;
+  
+  printf("the result is %i\n", sum);
       
   system("PAUSE");	
   return 0;
