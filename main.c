554791c4ded470e5896181693c0c4d76c9e0904d
hwaninging <hwanhee@sookmyung.ac.kr>
2025-10-02 16:00:53 +0900

    5. 산술계산기
    switch / if-else 둘 다 가능

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-// 4. 덧셈 프로그램 
+// 5. 산술계산기 (switch로 한 것) 
 int main(int argc, char *argv[])
 {
-  int x; 
-  int i; //for문 받아주기 위한 것 
-  int sum=0; //누적 더하기를 저장하는 저장소 
+  int a, b; //3가지변수 선언 및 scanf 선언 잘 해주기 
+  char op;  //연산자 
+  int result;  
    
-  printf("input a number: ");
-  scanf("%d", &x);
-  
-  //3가지 넣어야 하니까 뭘 반복할지부터 하기
-  for(i=0;i<=x;i++) //i 더할때마다 i 하나씩 증가 
-  sum = sum + i;
-  
-  printf("the result is %i\n", sum);
+  printf("enter the calculation: ");
+  scanf("%i %c %i", &a, &op, &b); //받는거  
+
+  switch(op)
+  {
+            case '+':
+                 result = a+b;
+                 break;
+            case '-':
+                  result = a-b;
+                  break;
+   }      
+
+  printf(" = %i\n", result);
       
   system("PAUSE");	
   return 0;
